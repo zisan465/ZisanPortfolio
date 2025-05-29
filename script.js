f5bb494ca13b2336846a215f2ceb5e060d8373f0
zisan465 <64661123+zisan465@users.noreply.github.com>
2025-05-29 22:55:57 +0600

    mian file added

---
+++ script.js
@@ -0,0 +1,79 @@
+  const videos = [
+    {
+      title: "Documentory Video",
+      description: "In this video, I used GeoLayers 3 in After Effects along with Premiere Pro.",
+      videoId: "5dhcqxO2tqE",
+      type: "Widescreen - 16:9"
+    },
+      {
+      title: "BSA Project",
+      description: "In this video, I used multiple layers with effects in Premiere Pro.",
+      videoId: "YgeY_vi9M7U",
+      type: "Widescreen - 16:9"
+    },
+    {
+      title: "Motion Graphics",
+      description: "In this video, I used Photoshop & After Effect.",
+      videoId: "plVjHhmg-Dk",
+      type: "Square - 1:1"
+    },
+    {
+      title: "Local Fruit Advertisement Video",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "f4KK75Uhwi0",
+      type: "Reels - 9:16"
+    },
+    {
+      title: "E-book Advertisement Video",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "xOcNeMc6fsM",
+      type: "Instagram Feed - 4:5"
+    },
+    {
+      title: "Coaching Center Promotion Video",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "K-jcnYkzVeI",
+      type: "Instagram Feed - 4:5"
+    },
+     {
+      title: "Product Promotion Video",
+      description: "In this video, I used Premier Pro & After Effect.",
+      videoId: "ky2sU_CWPRw",
+      type: "Reels - 9:16"
+    },
+  ];
+
+
+
+  
+  const ratioMap = {
+    "Widescreen - 16:9": "pt-[56.25%]",
+    "Reels - 9:16": "pt-[177.77%]",
+    "Instagram Feed - 4:5": "pt-[125%]",
+    "Square - 1:1": "pt-[100%]"
+  };
+
+
+
+   const testimonials = [
+    {
+      text: "This product changed my life! Highly recommended.",
+      author: "Jane Doe",
+      role: "CEO, Company A"
+    },
+    {
+      text: "Amazing service and support. I can't thank them enough.",
+      author: "John Smith",
+      role: "Freelancer"
+    },
+    {
+      text: "A truly outstanding experience from start to finish.",
+      author: "Mary Johnson",
+      role: "Marketing Manager"
+    },
+    {
+      text: "Their attention to detail and quality is unmatched.",
+      author: "Ahmed Khan",
+      role: "Developer"
+    }
+  ];
